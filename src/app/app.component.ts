3641c5a2cea7dbf4a6777819ce7e783e0a427b28
Rohan Thorat <rthorat1@binghamton.edu>
2022-11-30 22:22:07 -0500

    clean basecode, setup angular material and adding mat toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
