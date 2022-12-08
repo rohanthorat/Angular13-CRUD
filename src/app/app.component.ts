ce14a3b14e5968028f1fd793d5a23f9be430a4f5
Rohan Thorat <rthorat1@binghamton.edu>
2022-12-07 21:21:25 -0500

    edit button in action with product data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,6 +49,12 @@ export class AppComponent implements OnInit {
       });
   }
 
+  editProduct(row: any) {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row
+    });
+  }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -14,7 +14,8 @@ export class DialogComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private api: ApiService,
-    private dialogRef: MatDialogRef<DialogComponent>) {}
+    private dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public editData: any) {}
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -25,6 +26,10 @@ export class DialogComponent implements OnInit {
       comment: ['', Validators.required],
       date: ['', Validators.required]
     });
+  
+    if (this.editData) {
+      this.addDataToProductForm();
+    }
   }
 
   addProduct() {
@@ -42,6 +47,15 @@ export class DialogComponent implements OnInit {
       });
     }
     console.log(this.productForm.value);
+  }
+
+  addDataToProductForm() {
+    this.productForm.controls['productName'].setValue(this.editData.productName);
+    this.productForm.controls['category'].setValue(this.editData.category);
+    this.productForm.controls['comment'].setValue(this.editData.comment);
+    this.productForm.controls['price'].setValue(this.editData.price);
+    this.productForm.controls['date'].setValue(this.editData.date);
+    this.productForm.controls['freshness'].setValue(this.editData.freshness);
   }
 }
 
